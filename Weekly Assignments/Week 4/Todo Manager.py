5c98715cf72ae8927a2db4168c237594c8b6b5d7
crilab <62134235+crilab@users.noreply.github.com>
2024-11-06 14:09:53 +0000

    new week 4 assignment

--- Weekly Assignments/Week 4/Todo Manager.py
+++ Weekly Assignments/Week 4/Todo Manager.py
@@ -1,92 +1,58 @@
-'''
-Please be aware that students are no longer required to define their own functions, but it's good if they do.
-'''
-
 import os
 import json
 
+todos = []
 
-def clear_screen():
-    if os.name == 'nt':
-        os.system('cls')
-    elif os.name == 'posix':
-        os.system('clear')
+while True:
+    os.system('clear')
 
+    print('.: Todo Manger :.')
+    print('-----------------')
 
-def line():
-    print('------------------')
+    for i, todo in enumerate(todos):
+        print(i, '|', todo)
+    
+    print('-----------------')
+    print('A | Add todo')
+    print('C | Check todo')
+    print('D | Delete todo')
+    print('S | Save to file')
+    print('L | Load from file')
+    print('X | Exit program')
+    print('-----------------')
 
+    command = input('> ').upper()
 
-def load_todos():
-    with open('todos.json') as f:
-        return json.loads(f.read())
+    if command == 'A':
+        todo = input('Todo > ')
+        todos.append('[ ] ' + todo)
 
-
-def save_todos(todos):
-    with open('todos.json', 'w') as f:
-        f.write(json.dumps(todos))
-
-
-def get_index(todos):
-    while True:
+    elif command == 'C':
         try:
-            index = int(input('Index: '))
-            todos[index]
-            return index
-        except ValueError:
-            print('ERROR: invalid integer')
-        except IndexError:
-            print('ERROR: invalid index')
-
-
-def main():
-    todos = load_todos()
-    operation = ''
-
-    while True:
-        clear_screen()
-        
-        print('.: Todo Manager :.')
-        line()
-        for i, todo in enumerate(todos):
-            print(i, '|', todo)
-        line()
-        print('A | Add todo')
-        print('C | Check todo')
-        print('D | Delete todo')
-        print('X | Exit program')
-        line()
-
-        operation = input('> ').upper()
-        line()
-
-        if operation == 'A':
-            todo = input('Todo: ')
-            todos.append('[ ] ' + todo)
-            continue
-
-        elif operation == 'C':
-            '''
-            This operation (and delete) gets stuck if the todos list is empty. Many students may encounter this issue without realising it.
-
-            We should not penalize the students for this, but we should still recommend that they fix it.
-            '''
-            index = get_index(todos)
+            index = int(input('Index > '))
             todos[index] = '[X]' + todos[index][3:]
-        
-        elif operation == 'D':
-            index = get_index(todos)
+        except (ValueError, IndexError):
+            input('ERROR: invalid index')
+
+    elif command == 'D':
+        try:
+            index = int(input('Index > '))
             del todos[index]
-        
-        elif operation == 'X':
-            save_todos(todos)
-            exit(0)
-        
-        else:
-            print('ERROR: invalid operation')
-            line()
-            input('Press enter to continue...')
+        except (ValueError, IndexError):
+            input('ERROR: invalid index')
 
+    elif command == 'S':
+        f = open('todos.json', 'w')
+        f.write(json.dumps(todos))
+        f.close()
 
-if __name__ == '__main__':
-    main()
+    elif command == 'L':
+        f = open('todos.json')
+        todos = json.loads(f.read())
+        f.close()
+
+    elif command == 'X':
+        break
+
+    else:
+        input('ERROR: unknown command')
